af9f41b49e50c24cb437f7a3bfb2b9906603bc0f
ShantreJohnson <o2dn12@gmail.com>
2026-01-21 16:32:12 -0700

    Add initial Python script template for loan filter logic

---
+++ scripts/loan_filter.py
@@ -0,0 +1,22 @@
+"""
+loan_filter.py
+
+This module will contain the core logic for:
+- Loading loan data
+- Calculating debt-to-income (DTI)
+- Applying lending rules
+- Identifying high-risk applications
+
+The functions here will be developed as I progress through Python and Pandas training.
+"""
+
+def calculate_dti(income, debt):
+    """Placeholder function for DTI calculation (to be implemented)."""
+    return None
+
+def apply_rules(row):
+    """Placeholder function for applying risk rules (to be implemented)."""
+    return None
+
+if __name__ == "__main__":
+    print("loan_filter module loaded. Functionality will be added after training begins.")
